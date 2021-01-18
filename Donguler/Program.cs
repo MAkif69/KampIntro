2e49048d4675abee577bf955a3ede9be6d22d769
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-01-18 18:07:49 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kampı", "Programlama başlangıç için temel kurs", "Java"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+
+            //for (int i = 0; i < 10; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtkieti = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi)
+            {
+                Console.WriteLine("Kullanıcı ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+        }
+    }
+}
