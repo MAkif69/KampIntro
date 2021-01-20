0f179569a5b58ddddac2c9a9607e895ea0b1ddc8
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-01-21 00:03:32 +0300

    20 december 2021 C# Course Notes

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Akif", "Kadir", "Selim", "Burak" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Akif", "Kadir", "Selim", "Burak" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Fatih");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
