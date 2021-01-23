5611718043df0ba4a2790fb40db66cd806aa2ac0
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-01-23 23:55:20 +0300

    23 january 2021 C# Course Notes

---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12346";
+            musteri1.Adi = "Akif";
+            musteri1.Soyadi = "HASLAK";
+            musteri1.Soyadi = "12346";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "55555";
+            musteri2.SirketAdi = "Kodlam.io";
+            musteri2.VergiNo = "123456";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+         
+        }
+    }
+}
